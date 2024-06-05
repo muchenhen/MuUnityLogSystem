eeee6528f3b3b47434ad072db9c2201990fcfe68
muchenhen <44308876+muchenhen@users.noreply.github.com>
2024-06-05 20:34:10 +0800

    修改Logger，根据设定的LogSetting等级进行输出log

--- MuLogSystem/Assets/LogSystem/Logger.cs
+++ MuLogSystem/Assets/LogSystem/Logger.cs
@@ -1,6 +1,7 @@
 ﻿// 定义日志宏
 
 using System;
+using UnityEditor;
 
 namespace LogSystem
 {
@@ -21,16 +22,38 @@ namespace LogSystem
         // 日志写入器
         private static LogWriter _logWriter;
 
+        // 日志输出等级
+        private static LogLevel LogOutputLevel { get; set; } = LogLevel.Display;
+
+
         // 初始化日志系统
         public static void Initialize()
         {
             _logWriter = new LogWriter();
+
+            // 加载LogSettings资源文件
+            string[] guids = AssetDatabase.FindAssets("t:LogSettings");
+            if (guids.Length > 0)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guids[0]);
+                LogSettings settings = AssetDatabase.LoadAssetAtPath<LogSettings>(path);
+                if (settings != null)
+                {
+                    LogOutputLevel = settings.logOutputLevel;
+                }
+            }
         }
 
         // 写入日志
         // ReSharper disable once MemberCanBePrivate.Global
         public static void Log(LogLevel level, string message, params object[] args)
         {
+            // 只有当日志级别高于指定的输出级别时才进行输出
+            if (level > LogOutputLevel)
+            {
+                return;
+            }
+
             string formattedMessage = string.Format(message, args);
             _logWriter.WriteLog(level, formattedMessage);
 
@@ -55,6 +78,24 @@ namespace LogSystem
         {
             Log(LogLevel.Error, message, args);
         }
+
+#if UNITY_EDITOR
+        [UnityEditor.InitializeOnLoadMethod]
+        private static void OnInitializeOnLoad()
+        {
+            UnityEditor.BuildPlayerWindow.RegisterBuildPlayerHandler(OnBuildPlayer);
+        }
+
+        private static void OnBuildPlayer(BuildPlayerOptions options)
+        {
+            string[] guids = UnityEditor.AssetDatabase.FindAssets("t:LogSettings");
+            if (guids.Length > 0)
+            {
+                string path = UnityEditor.AssetDatabase.GUIDToAssetPath(guids[0]);
+                options.assetBundleManifestPath = path;
+            }
+        }
+#endif
     }
 
     public class FatalException : Exception
